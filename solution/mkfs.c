f579f15a19939d76ad8bf58a81d65d84567a33ca
SDoyiakos <stevendoyiakos@gmail.com>
2024-11-25 18:10:34 -0600

    simple raid mode parsing

--- solution/mkfs.c
+++ solution/mkfs.c
@@ -7,22 +7,40 @@ int disk_image_count; // The number of disk images
 int inode_count; // The number of inodes
 int block_count; // The number of data blocks
 
+int parseRaid(char* str) {
+	int raid_val;
+	
+	// The length of the int will be 1 char
+	if(strlen(str) != 1) {
+		return -1;
+	}
+
+	raid_val = *str - '0'; // Convert char to int
+	if(raid_val != 0 && raid_val != 1) { // We are only doing 0 and 1	
+		return -1;
+	}
+
+	return raid_val;
+	
+}
+
 int parseArgs(int argc, char* argv[]) {
 	char* curr_arg;
 	
 	// Iterate over all args
-	for(int i = 1;i < argc;i++) {
+	for(int i = 1;i < argc - 1;i+=2) {
 		curr_arg = argv[i];
-		if(strcmp(curr_arg, "-r")) {
-			// Code to handle raid flag
+		if(strcmp(curr_arg, "-r") == 0) {
+			raid_mode = parseRaid(argv[i+1]);
+			printf("The raid mode is %d\n", raid_mode);
 		}
-		else if(strcmp(curr_arg, "-d")) {
+		else if(strcmp(curr_arg, "-d") == 0) {
 			// Code to handle disk flag(s)
 		}
-		else if(strcmp(curr_arg, "-i")) {
+		else if(strcmp(curr_arg, "-i") == 0) {
 			// Code to handle inode count flag
 		}
-		else if(strcmp(curr_arg, "-b")) {
+		else if(strcmp(curr_arg, "-b") == 0) {
 			// Code to handle data block count flag	
 		}
 		else {
