dc8a6a70a21f7ea7769c2e1f5a71255e7579934c
Phoenix <pbpham@wisc.edu>
2024-11-27 12:01:54 -0600

    made methods stubs

--- solution/wfs.c
+++ solution/wfs.c
@@ -39,54 +39,15 @@ typedef unsigned long  u_long;
 #include <sys/time.h>
 #include <sys/param.h>
 
-#define G_PREFIX   "org"
-#define G_KAUTH_FILESEC_XATTR G_PREFIX ".apple.system.Security"
-#define A_PREFIX   "com"
-#define A_KAUTH_FILESEC_XATTR A_PREFIX ".apple.system.Security"
-#define XATTR_APPLE_PREFIX   "com.apple."
 
 static int wfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			   off_t offset, struct fuse_file_info *fi)
 {
-	DIR *dp;
-	struct dirent *de;
-
-	(void) offset;
-	(void) fi;
-
-	dp = opendir(path);
-	if (dp == NULL)
-		return -errno;
-
-	while ((de = readdir(dp)) != NULL) {
-		struct stat st;
-		memset(&st, 0, sizeof(st));
-		st.st_ino = de->d_ino;
-		st.st_mode = de->d_type << 12;
-		if (filler(buf, de->d_name, &st, 0))
-			break;
-	}
-
-	closedir(dp);
 	return 0;
 }
 
 static int wfs_mknod(const char *path, mode_t mode, dev_t rdev)
 {
-	int res;
-
-	/* On Linux this could just be 'mknod(path, mode, rdev)' but this
-	   is more portable */
-	if (S_ISREG(mode)) {
-		res = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
-		if (res >= 0)
-			res = close(res);
-	} else if (S_ISFIFO(mode))
-		res = mkfifo(path, mode);
-	else
-		res = mknod(path, mode, rdev);
-	if (res == -1)
-		return -errno;
 
 	return 0;
 }
@@ -99,23 +60,11 @@ static int wfs_mkdir(const char *path, mode_t mode)
 
 static int wfs_unlink(const char *path)
 {
-	int res;
-
-	res = unlink(path);
-	if (res == -1)
-		return -errno;
-
 	return 0;
 }
 
 static int wfs_rmdir(const char *path)
 {
-	int res;
-
-	res = rmdir(path);
-	if (res == -1)
-		return -errno;
-
 	return 0;
 }
 
@@ -123,65 +72,20 @@ static int wfs_rmdir(const char *path)
 static int wfs_read(const char *path, char *buf, size_t size, off_t offset,
 			struct fuse_file_info *fi)
 {
-	int fd;
-	int res;
-
-	(void) fi;
-	fd = open(path, O_RDONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = pread(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
-
-	close(fd);
-	return res;
+	return 0;
 }
 
 static int wfs_write(const char *path, const char *buf, size_t size,
 			 off_t offset, struct fuse_file_info *fi)
 {
-	int fd;
-	int res;
-
-	(void) fi;
-	fd = open(path, O_WRONLY);
-	if (fd == -1)
-		return -errno;
-
-	res = pwrite(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
-
-	close(fd);
-	return res;
+	return 0;
 }
 
 
 static int wfs_getattr(const char *path, const char *name, char *value, size_t size,
 				  uint32_t position)
 {
-	int res;
-
-	if (strcmp(name, A_KAUTH_FILESEC_XATTR) == 0) {
-
-		char new_name[MAXPATHLEN];
-
-		memcpy(new_name, A_KAUTH_FILESEC_XATTR, sizeof(A_KAUTH_FILESEC_XATTR));
-		memcpy(new_name, G_PREFIX, sizeof(G_PREFIX) - 1);
-
-		res = getxattr(path, new_name, value, size, position, XATTR_NOFOLLOW);
-
-	} else {
-		res = getxattr(path, name, value, size, position, XATTR_NOFOLLOW);
-	}
-
-	if (res == -1) {
-		return -errno;
-	}
-
-	return res;
+	return 0;
 }
 
 
