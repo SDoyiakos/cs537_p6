418377181bd3c80da8aa54997df5cdd850f65d4e
Phoenix Bach Pham <pbpham@wisc.edu>
2024-11-30 12:47:49 -0600

    added commented out code

--- solution/wfs.c
+++ solution/wfs.c
@@ -33,6 +33,149 @@ static struct wfs_inode **roots;
 //	return 0;
 //
 //}
+
+// Find the inode with number inum on device dev
+// and return the in-memory copy. Does not lock
+// the inode and does not read it from disk.
+//static struct inode*
+//iget(uint dev, uint inum)
+//{ 
+//  struct inode *ip, *empty;
+//  
+//  acquire(&icache.lock);
+//  
+//  // Is the inode already cached?
+//  empty = 0;
+//  for(ip = &icache.inode[0]; ip < &icache.inode[NINODE]; ip++){
+//    if(ip->ref > 0 && ip->dev == dev && ip->inum == inum){
+//      ip->ref++;
+//      release(&icache.lock);
+//      return ip;
+//    }
+//    if(empty == 0 && ip->ref == 0)    // Remember empty slot.
+//      empty = ip;
+//  }
+//
+//  // Recycle an inode cache entry.
+//  if(empty == 0)
+//    panic("iget: no inodes");
+//
+//  ip = empty;
+//  ip->dev = dev;
+//  ip->inum = inum;
+//  ip->ref = 1;
+//  ip->valid = 0;
+//  release(&icache.lock);
+//
+//  return ip;
+//}
+
+// Look for a directory entry in a directory.
+// If found, set *poff to byte offset of entry.
+//struct inode*
+//dirlookup(struct inode *dp, char *name, uint *poff)
+//{ 
+//  uint off, inum;
+//  struct dirent de;
+//  
+//  if(dp->type != T_DIR)
+//    panic("dirlookup not DIR");
+//  
+//  for(off = 0; off < dp->size; off += sizeof(de)){
+//    if(readi(dp, (char*)&de, off, sizeof(de)) != sizeof(de))
+//      panic("dirlookup read");
+//    if(de.inum == 0)
+//      continue;
+//    if(namecmp(name, de.name) == 0){
+//      // entry matches path element
+//      if(poff)
+//        *poff = off;
+//      inum = de.inum;
+//      return iget(dp->dev, inum);
+//    }
+//  }
+//  
+//  return 0;
+//}
+
+
+ // Copy the next path element from path into name.
+// Return a pointer to the element following the copied one.
+// The returned path has no leading slashes,
+// so the caller can check *path=='\0' to see if the name is the last one.
+// If no name to remove, return 0.
+//
+// Examples:
+//   skipelem("a/bb/c", name) = "bb/c", setting name = "a"
+//   skipelem("///a//bb", name) = "bb", setting name = "a"
+//   skipelem("a", name) = "", setting name = "a"
+//   skipelem("", name) = skipelem("////", name) = 0
+//
+//static char*
+//skipelem(char *path, char *name)
+//{ 
+//  char *s;
+//  int len;
+//  
+//  while(*path == '/')
+//    path++;
+//  if(*path == 0)
+//    return 0;
+//  s = path;
+//  while(*path != '/' && *path != 0)
+//    path++;
+//  len = path - s;
+//  if(len >= DIRSIZ)
+//    memmove(name, s, DIRSIZ);
+//  else {
+//    memmove(name, s, len);
+//    name[len] = 0;
+//  }
+//  while(*path == '/')
+//    path++;
+//  return path;
+//}
+
+ // Look up and return the inode for a path name.
+// If parent != 0, return the inode for the parent and copy the final
+// path element into name, which must have room for DIRSIZ bytes.
+// Must be called inside a transaction since it calls iput().
+//static struct inode*
+//namex(char *path, int nameiparent, char *name)
+//{
+//  struct inode *ip, *next;
+//
+//  if(*path == '/')
+//    ip = iget(ROOTDEV, ROOTINO);
+//  else
+//    ip = idup(myproc()->cwd);
+//
+//  while((path = skipelem(path, name)) != 0){
+//    ilock(ip);
+//    if(ip->type != T_DIR){
+//      iunlockput(ip);
+//      return 0;
+//    }
+//    if(nameiparent && *path == '\0'){
+//      // Stop one level early.
+//      iunlock(ip);
+//      return ip;
+//    }
+//    if((next = dirlookup(ip, name, 0)) == 0){
+//      iunlockput(ip);
+//      return 0;
+//    }
+//    iunlockput(ip);
+//    ip = next;
+//  }
+//  if(nameiparent){
+//    iput(ip);
+//    return 0;
+//  }
+//  return ip;
+//}
+
+
 static int wfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			   off_t offset, struct fuse_file_info *fi)
 {
