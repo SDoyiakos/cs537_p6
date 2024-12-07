888a1898e69fd99126adf3953c8a5dd16e76ddea
SDoyiakos <stevendoyiakos@gmail.com>
2024-12-07 13:19:52 -0600

    File maker

---
+++ solution/createFile.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+extern errno;
+int main(int argc, char* argv[]) {
+	int fd;
+	if(argc != 2) {
+		printf("Error, you need to have one arg\n");
+		return 1;
+	}
+
+	fd = open ("output.txt" ,O_CREAT|O_RDWR);
+	char entry = 'a';
+	printf("Writing %d bytes\n", atoi(argv[1]));
+	for(int i =0; i < atoi(argv[1]);i++) {
+		write(fd, &entry, 1);
+	}
+	printf("Wrote to file\n");
+	
+	close(fd);
+	return 0;
+}
